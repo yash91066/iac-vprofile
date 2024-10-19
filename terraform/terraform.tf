ac2bf975081c0ea63d142402074fb30f87ad5947
yash91066 <myadwords417@gmail.com>
2024-10-18 19:30:31 -0600

    plan has been done

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -27,9 +27,9 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "gitopsterrastate"
+    bucket = "gitops2307"
     key    = "terraform.tfstate"
-    region = "us-east-2"
+    region = "ca-central-1"
   }
 
   required_version = "~> 1.6.3"
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,11 +1,11 @@
 variable "region" {
   description = "AWS region"
   type        = string
-  default     = "us-east-2"
+  default     = "ca-central-1"
 }
 
 variable "clusterName" {
-  description = "Name of the EKS cluster"
+  description = "The name of the EKS cluster"
   type        = string
-  default     = "kitops-eks"
+  default     = "mydevops.yashcanada.space"
 }
